cc17037c3828e7b1f4e5b8e80e13748dd6deacba
Gopi <gopikrishna.gummadidala@gmail.com>
2021-05-08 21:32:35 +0200

    routing added

--- pwa-demo/src/app/app-routing.module.ts
+++ pwa-demo/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { GeneralComponent } from "./general/general.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "general",
+    component: GeneralComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
